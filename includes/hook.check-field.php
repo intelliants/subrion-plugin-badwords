2dc41a5dc9020cfb313ed1ca1d738b2a22cba6c6
Vasily Bezruchkin <vbezruchkin@gmail.com>
2018-08-30 12:26:53 +0600

    4.2.0 (#1)
    * adapt to 4.2.0
    
    * minor improvement

--- includes/hook.check-field.php
+++ includes/hook.check-field.php
@@ -24,32 +24,28 @@
  *
  ******************************************************************************/
 
-if (in_array($value['type'], array(iaField::TEXT, iaField::TEXTAREA)))
-{
-	static $badWordsRegExp;
+if (in_array($field['type'], [iaField::TEXT, iaField::TEXTAREA])) {
 
-	if (is_null($badWordsRegExp))
-	{
-		$list = explode(',', $iaCore->get('bad_word_list'));
-		$array = array();
-		foreach ($list as $word)
-		{
-			if ($word = trim($word))
-			{
-				$array[] = '\b' . preg_quote($word) . '\b';
-			}
-		}
-		empty($array) || $array = array_unique($array);
-		$badWordsRegExp = $array ? '/' . implode('|', $array) . '/i' : '';
-	}
+    static $badWordsRegExp;
 
-	if ($badWordsRegExp)
-	{
-		$matches = array();
-		if (preg_match_all($badWordsRegExp, $item, $matches))
-		{
-			$error = true;
-			$msg[] = iaLanguage::getf('error_bad_words', array('field' => iaLanguage::get('field_' . $field_name), 'words' => implode(', ', $matches[0])));
-		}
-	}
+    if (is_null($badWordsRegExp)) {
+        $list = explode(',', $iaCore->get('bad_word_list'));
+        $array = [];
+        foreach ($list as $word) {
+            if ($word = trim($word)) {
+                $array[] = '\b' . preg_quote($word) . '\b';
+            }
+        }
+        empty($array) || $array = array_unique($array);
+        $badWordsRegExp = $array ? '/' . implode('|', $array) . '/i' : '';
+    }
+
+
+    if ($badWordsRegExp) {
+        $matches = array();
+
+        if (preg_match_all($badWordsRegExp, $value, $matches)) {
+            $errors[] = iaLanguage::getf('error_bad_words', array('field' => iaLanguage::get('field_' . $field['item'] . '_' . $field['name']), 'words' => implode(', ', $matches[0])));
+        }
+    }
 }
